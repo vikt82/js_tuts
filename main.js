78dc9928498e0808a6be87364a9339d5d04edb47
Viktor <victor.p@wecandevelopit.com>
2016-08-17 17:00:20 +0300

    1

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 "use strict";
+
+// var name = "Василий"
+// var admin = name;
+var admin, name;
+    name = "Василий";
+    admin = name;
+console.log(admin);
+
+var ourPlanet = "Земля",
+    visitorName = "Вася";
+
+console.log(ourPlanet);
+console.log(visitorName)
+
+console.log(1 / 0); // При делении на 0 дает бесконечность;
+
+var i = 7;
+    i++;
+console.log(i);
+
+var i = 2;
+var a = ++i;
+console.log(a);
